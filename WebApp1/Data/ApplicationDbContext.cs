4fed7427831367d373a6e68736d73b2ad96548e6
Mircea Ciprian <mircea.m.ciprian@gmail.com>
2024-08-11 18:02:01 +0300

    Initial commit of Presidential Elections project

---
+++ WebApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp1.Models;
+
+namespace WebApp1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<MyUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebApp1/Models/MyUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp1.Models
+{
+    public class MyUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Genre { get; set; }
+        public Boolean voted = false;
+    }
+}
